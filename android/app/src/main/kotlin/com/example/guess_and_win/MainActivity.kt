97f68dab8711ae1fddfe1fd8bba449fce238e08e
Vishal Sharma <vsunite3@gmail.com>
2022-02-08 10:32:16 +0530

    Initial commit : Imported google fonts, Set initial route

---
+++ android/app/src/main/kotlin/com/example/guess_and_win/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.guess_and_win
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:guess_and_win/screens/home_page.dart';
+import 'package:guess_and_win/screens/login_screen.dart';
+import 'package:guess_and_win/utilities/routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginScreen(),
+        MyRoutes.homeRoute: (context) => HomeScreen(),
+        MyRoutes.loginRoute: (context) => LoginScreen(),
+      },
+    );
+  }
+}
