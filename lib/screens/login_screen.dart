d5fd062572dbf8527a6b14a7a4885ebe12918547
Vishal Sharma <vsunite3@gmail.com>
2022-02-08 11:27:01 +0530

    Basic UI for Login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:guess_and_win/utilities/routes.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Welcome')),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Phone number'),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: Text('Login'),
+              onPressed: () {
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
